11af8413eada6807814ea088ad11c987d064a7e8
Cknyt <100312336+Cknyt@users.noreply.github.com>
2024-08-17 17:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import VectorSource from 'ol/source/Vector';
-import VectorLayer from 'ol/layer/Vector';
-import LineString from 'ol/geom/LineString';
-import Draw from 'ol/interaction/Draw';
-import { getLength } from 'ol/sphere';
-
-const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
-    view: new View({
-        center: [-3.749220, 40.463667], // España (longitud, latitud)
-        zoom: 5
-    })
-});
-
-const source = new VectorSource();
-const vector = new VectorLayer({
-    source: source,
-    style: {
-        'stroke-color': 'black',
-        'stroke-width': 2
-    }
-});
-map.addLayer(vector);
-
+let map;
+let draw; 
+let source;
+let vector;
 let sketch;
 let distanciaTotal = 0;
 
-const draw = new Draw({
-    source: source,
-    type: 'LineString'
-});
-map.addInteraction(draw);
+function initMap() {
+    map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([-3.749220, 40.463667]), // España (longitud, latitud)
+            zoom: 5
+        })
+    });
 
-draw.on('drawstart', function (event) {
-    sketch = event.feature; 
-    distanciaTotal = 0; 
-});
+    source = new ol.source.Vector(); 
+    vector = new ol.layer.Vector({
+        source: source,
+        style: new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'black',
+                width: 2
+            })
+        })
+    });
+    map.addLayer(vector);
 
-draw.on('drawend', function (event) {
-    const geometry = sketch.getGeometry(); 
-    const distance = getLength(geometry); 
-    distanciaTotal += distance / 1000; 
-    document.getElementById('distancia').textContent = distanciaTotal.toFixed(2);
-});
+    draw = new ol.interaction.Draw({
+        source: source,
+        type: 'LineString'
+    });
+    map.addInteraction(draw);
+
+    draw.on('drawstart', function (event) {
+        sketch = event.feature;
+        distanciaTotal = 0;
+    });
+
+    draw.on('drawend', function (event) {
+        const geometry = sketch.getGeometry();
+        const distance = ol.sphere.getLength(geometry);
+        distanciaTotal += distance / 1000;
+        document.getElementById('distancia').textContent = distanciaTotal.toFixed(2);
+    });
+}
+
+initMap();
